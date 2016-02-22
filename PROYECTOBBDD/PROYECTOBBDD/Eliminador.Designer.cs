5ec44f1074c63858d131e3b663f7b41419c400a3
jguilindro <julio.guilindro@gmail.com>
2016-02-22 03:10:42 -0500

    Eliminar Clientes Completo

--- PROYECTOBBDD/PROYECTOBBDD/Eliminador.Designer.cs
+++ PROYECTOBBDD/PROYECTOBBDD/Eliminador.Designer.cs
@@ -28,47 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Eliminar = new System.Windows.Forms.Button();
             this.Texto = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Eliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // Eliminar
-            // 
-            this.Eliminar.Location = new System.Drawing.Point(181, 107);
-            this.Eliminar.Name = "Eliminar";
-            this.Eliminar.Size = new System.Drawing.Size(75, 23);
-            this.Eliminar.TabIndex = 0;
-            this.Eliminar.Text = "Eliminar";
-            this.Eliminar.UseVisualStyleBackColor = true;
-            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
             // 
             // Texto
             // 
             this.Texto.AutoSize = true;
-            this.Texto.Location = new System.Drawing.Point(53, 57);
+            this.Texto.Location = new System.Drawing.Point(29, 72);
             this.Texto.Name = "Texto";
             this.Texto.Size = new System.Drawing.Size(41, 13);
-            this.Texto.TabIndex = 1;
+            this.Texto.TabIndex = 0;
             this.Texto.Text = "Opcion";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 57);
+            this.textBox1.Location = new System.Drawing.Point(274, 72);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(310, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(179, 155);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 2;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
             // 
             // Eliminador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 162);
+            this.ClientSize = new System.Drawing.Size(459, 233);
+            this.Controls.Add(this.Eliminar);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Texto);
-            this.Controls.Add(this.Eliminar);
             this.Name = "Eliminador";
             this.Text = "Eliminador";
+            this.Load += new System.EventHandler(this.Eliminador_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,8 +77,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button Eliminar;
         private System.Windows.Forms.Label Texto;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Eliminar;
     }
 }
